e34022da3d6833df29900b2ab652a66fc7f56db3
Suissa <jnascimento@gmail.com>
2017-07-04 06:33:57 -0300

    Finalizando refatoração certa do CPF

--- desafios/reduce/cpf.definicao.md
+++ desafios/reduce/cpf.definicao.md
@@ -356,3 +356,95 @@ const validate = ( cpfFull ) => {
 }
 
 ``` 
+
+Vamos melhorar mais um pouco, dessa forma:
+
+
+<br>
+
+```js
+const NOT = ( x ) => !x
+
+const isSameDigitsCPF = ( cpfFull ) => 
+  isIn( generateArray( 10 ).map( generateSequenceSize11 ) )( cpfFull )
+
+const isValidCPF = ( cpfFull ) => ( firstDigit, secondDigit ) =>
+  isEqual( getTwoLastDigits( cpfFull ) )
+        ( mergeDigits( firstDigit, secondDigit ) )
+
+const getValidationDigit = ( total ) => ( cpf ) =>
+  testDigit( mod11( getSumOfMultiplication( cpf, total ) ) )
+
+const validate = ( cpfFull ) => {
+  
+  const cpf = getCpfToCheck( cpfFull )
+  const firstDigit = getValidationDigit( 10 )( cpf ) ) )
+  const secondDigit = getValidationDigit( 11 )(  cpf.concat( firstDigit ) ) ) )
+
+  return  NOT( isSameDigitsCPF( cpfFull ) ) && 
+          isValidCPF( cpfFull )( firstDigit, secondDigit )
+}
+
+``` 
+
+<br>
+
+Agora sim temos nosso código final assim:
+
+<br>
+
+```js
+
+const mod11 = ( num ) => num % 11 
+const NOT = ( x ) => !x
+const isEqual = ( a ) => ( b ) => b === a 
+const mergeDigits = ( num1, num2 ) => `${num1}${num2}`
+const getTwoLastDigits = ( cpf ) => `${cpf[ 9 ]}${cpf[ 10 ]}`
+const getCpfToCheckInArray = ( cpf ) => cpf.substr( 0, 9 ).split( '' )
+const generateArray = ( length ) => Array.from( { length }, ( v, k ) => k )
+const isIn = ( list ) => ( value ) => 
+  list.findIndex( v => value === v ) >= 0
+
+const generateStringSequence = ( times ) => ( char ) => 
+  ( `${char}`.repeat( times ) )
+
+const getDigit = ( num ) => 
+  ( num > 1 )
+    ? 11 - num
+    : 0
+
+const toSumOfMultiplication = ( total ) => ( result, num, i ) => 
+  result + ( num * total-- )
+
+const getSumOfMultiplication = ( list, total ) => 
+  list.reduce( toSumOfMultiplication( total ), 0 )
+
+
+const isSameDigitsCPF = ( cpfFull ) => 
+  isIn( generateArray( 10 ).map( generateStringSequence( 11 ) ) )( cpfFull )
+
+const isValidCPF = ( cpfFull ) => ( firstDigit, secondDigit ) =>
+  isEqual( getTwoLastDigits( cpfFull ) )
+         ( mergeDigits( firstDigit, secondDigit ) )
+
+const getValidationDigit = ( total ) => ( cpf ) =>
+  getDigit( mod11( getSumOfMultiplication( cpf, total ) ) )
+
+const validate = ( cpfFull ) => {
+  
+  const cpf = getCpfToCheckInArray( cpfFull )
+  const firstDigit = getValidationDigit( 10 )( cpf )
+  const secondDigit = getValidationDigit( 11 )( cpf.concat( firstDigit ) )
+
+  return  NOT( isSameDigitsCPF( cpfFull ) ) && 
+          isValidCPF( cpfFull )( firstDigit, secondDigit )
+}
+
+const CPFS = [ 
+  '04998264931', '03506838326','04864713901',
+  '03506838321', '22222222222', '00000000000' 
+  ]
+
+CPFS.forEach( ( cpf ) => console.log( `${cpf}: ${validate( cpf )}` ) )
+
+```
--- desafios/reduce/cpf.passo.a.passo.js
+++ desafios/reduce/cpf.passo.a.passo.js
@@ -1,17 +1,20 @@
 const mod11 = ( num ) => num % 11 
+const NOT = ( x ) => !x
+const isEqual = ( a ) => ( b ) => b === a 
+const mergeDigits = ( num1, num2 ) => `${num1}${num2}`
+const getTwoLastDigits = ( cpf ) => `${cpf[ 9 ]}${cpf[ 10 ]}`
+const getCpfToCheckInArray = ( cpf ) => cpf.substr( 0, 9 ).split( '' )
+const generateArray = ( length ) => Array.from( { length }, ( v, k ) => k )
+const isIn = ( list ) => ( value ) => 
+  list.findIndex( v => value === v ) >= 0
 
-const testDigit = ( num ) => 
-  ( num < 2 )
-    ? 0
-    : 11 - num
+const generateStringSequence = ( times ) => ( char ) => 
+  ( `${char}`.repeat( times ) )
 
-const getCpfToCheck = ( cpf ) => 
-  ( cpf.map )
-    ? cpf.slice( 0, 9 )
-    : cpf.substr( 0, 9 ).split( '' )
-
-const CPF1 = '04998264931'
-let total = 10
+const getDigit = ( num ) => 
+  ( num > 1 )
+    ? 11 - num
+    : 0
 
 const toSumOfMultiplication = ( total ) => ( result, num, i ) => 
   result + ( num * total-- )
@@ -20,36 +23,25 @@ const getSumOfMultiplication = ( list, total ) =>
   list.reduce( toSumOfMultiplication( total ), 0 )
 
 
-const getTwoLastDigits = ( cpf ) => `${cpf.charAt( 9 )}${cpf.charAt( 10 )}` 
-const mergeDigits = ( num1, num2 ) => `${num1}${num2}`
+const isSameDigitsCPF = ( cpfFull ) => 
+  isIn( generateArray( 10 ).map( generateStringSequence( 11 ) ) )( cpfFull )
 
-const isEqual = ( a ) => ( b ) => b === a
+const isValidCPF = ( cpfFull ) => ( firstDigit, secondDigit ) =>
+  isEqual( getTwoLastDigits( cpfFull ) )
+         ( mergeDigits( firstDigit, secondDigit ) )
 
-
-const generateStringSequence = ( times ) => ( char ) => `${char}`.repeat( times )
-const generateArray = ( length ) => Array.from( { length }, ( v, k ) => k )
-
-const sequenceFrom0To9 = generateArray( 10 )
-const generateSequenceSize11 = generateStringSequence( 11 )
-
-const isIn = ( list ) => ( value ) => 
-  list.findIndex( v => value === v ) >= 0
+const getValidationDigit = ( total ) => ( cpf ) =>
+  getDigit( mod11( getSumOfMultiplication( cpf, total ) ) )
 
 const validate = ( cpfFull ) => {
   
-  const cpf = getCpfToCheck( cpfFull )
-  const firstDigit = testDigit( mod11( getSumOfMultiplication( cpf, 10 ) ) )
-  const secondDigit = testDigit( mod11( 
-                                    getSumOfMultiplication( cpf.concat( firstDigit ), 
-                                                            11 ) ) )
+  const cpf = getCpfToCheckInArray( cpfFull )
+  const firstDigit = getValidationDigit( 10 )( cpf )
+  const secondDigit = getValidationDigit( 11 )( cpf.concat( firstDigit ) )
 
-  return !isIn( generateArray( 10 ).map( generateSequenceSize11 ) )( cpfFull ) &&
-    isEqual( getTwoLastDigits( cpfFull ) )( mergeDigits( firstDigit, secondDigit ) )
-
+  return  NOT( isSameDigitsCPF( cpfFull ) ) && 
+          isValidCPF( cpfFull )( firstDigit, secondDigit )
 }
-
-// console.log( CPF1, validate( CPF1 ) ) //true
-
 
 const CPFS = [ 
   '04998264931', '03506838326','04864713901',
